166659733c420673836c7e9fb36d202e6ed429dc
vipulmalviya <vipulmalviya9987@gmail.com>
2023-07-30 18:13:40 +0530

    cursor added

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-// gsap.registerPlugin(ScrollTrigger);
 
-// // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-// const locoScroll = new LocomotiveScroll({
-//   el: document.querySelector(".wraper"),
-//   smooth: true
-// });
-// // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-// locoScroll.on("scroll", ScrollTrigger.update);
-
-// // tell ScrollTrigger to use these proxy methods for the ".wraper" element since Locomotive Scroll is hijacking things
-// ScrollTrigger.scrollerProxy(".wraper", {
-//   scrollTop(value) {
-//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-//   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-//   getBoundingClientRect() {
-//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-//   },
-//   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-//   pinType: document.querySelector(".wraper").style.transform ? "transform" : "fixed"
-// });
 
 
 gsap.to(".p1a", {
@@ -85,5 +64,45 @@ closesym.addEventListener('click', () => {
     menubar.classList.toggle('active');
     closesym.style.display = "none";
     menu.style.display = "block";
-  
+
 })
+
+
+
+
+function skeowCircle() {
+    var defultX = 1
+    var defultY = 1
+    var X = 0
+    var Y = 0
+    var timeout
+    window.addEventListener("mousemove", function (dets) {
+        clearTimeout(timeout); 
+
+        defultX = gsap.utils.clamp(0.8, 1.2, X - dets.clientX)
+        defultY = gsap.utils.clamp(0.8, 1.2, Y - dets.clientY)
+
+        X = dets.clientX
+        Y = dets.clientY
+
+        mousefollow(defultX, defultY)
+    
+        timeout = setTimeout(() => {
+        document.querySelector(".circle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1,1)`
+        }, 200);
+
+    })
+}
+
+function mousefollow(defultX,defultY) {
+    window.addEventListener("mousemove", function (dets) {
+        document.querySelector(".circle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${defultX},${defultY})`
+    })
+}
+
+
+
+skeowCircle()
+
+
+
